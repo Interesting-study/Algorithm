bcc54f41aab21a97ffd33191d047255874e940bb
Slowth-KIM <rt.slowth@gmail.com>
2021-01-06 22:00:46 +0900

    modify 819 & solve 937

--- leetcode/819) Most Common Word.py
+++ leetcode/819) Most Common Word.py
@@ -6,3 +6,5 @@ class Solution:
         words = [word for word in re.sub(r'[^\w]', ' ', paragraph).lower().split() if word not in banned]
         counts = collections.Counter(words)
         return counts.most_common(1)[0][0]
+
+#https://leetcode.com/problems/most-common-word/
