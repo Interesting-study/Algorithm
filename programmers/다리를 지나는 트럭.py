a3e8a02455e692823a5c5d5e89f8b85f5c5659af
Slowth-KIM <rt.slowth@gmail.com>
2021-03-29 21:43:07 +0900

    다리를 지나느 트럭 - 건널 트럭까지 구함, 시간 구하는 로직 다시 체크

--- programmers/다리를 지나는 트럭.py
+++ programmers/다리를 지나는 트럭.py
@@ -1,15 +1,22 @@
 from collections import deque
 def solution(bridge_length, weight, truck_weights):
-  answer = 0
-  truck_weights = deque(truck_weights)
-  pass_bridge = deque([])
-  times = 0
-    
-    
+    truck_weights = deque(truck_weights)
+    pass_bridge = deque([])
+    times = 0
 
-  while sum(pass_bridge) <= 10:
-    pass_bridge.append(truck_weights.popleft())
-  truck_weights.appendleft(pass_bridge.pop())
-  print(pass_bridge, truck_weights)
-
-# 무게가 10kg까지, 다리를 건널 트럭
+    while len(truck_weights):
+        while len(truck_weights):
+            pass_bridge.append(truck_weights.popleft())
+            if sum(pass_bridge) >= weight:
+                break
+    
+        if sum(pass_bridge) > weight or len(pass_bridge) > 2:
+            truck_weights.appendleft(pass_bridge.pop())
+        print(pass_bridge, times)
+        #다리를 건널 트럭을 골라냄, pass_bridge의 sum이 weight 보다 작거나 같아야 함    
+    
+        while len(pass_bridge):
+            times += 1
+            if times >= bridge_length:
+                pass_bridge.popleft()
+        print(pass_bridge, times)
