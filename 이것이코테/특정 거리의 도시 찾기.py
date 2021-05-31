f9707923f11cc587f05f48c4fb17bb31e45d0c53
Slowth-KIM <rt.slowth@gmail.com>
2021-05-31 15:09:06 +0900

    특정 거리의 도시 찾기 완료

---
+++ 이것이코테/특정 거리의 도시 찾기.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+n, m, k, x = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+
+distance = [-1] * [n+1]
+distance[x] = 0
+
+q = deque([x])
+
+while q:
+    now = q.popleft()
+
+    for next_city in graph[now]:
+        if distance[next_city] == -1:
+            distance[next_city] = distance[now] + 1
+            q.append(next_city)
+
+check = False
+for i in range(1, n+1):
+    if distance[i] == k:
+        print(i)
+        check = True
+
+if not check:
+    print(-1)
+
