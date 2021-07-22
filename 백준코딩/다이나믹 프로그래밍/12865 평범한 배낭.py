de06f3940238bff554560f4f1ba0ca23c2b724d9
Slowth-KIM <rt.slowth@gmail.com>
2021-07-22 14:34:06 +0900

    12865 평범한 배낭 해결

---
+++ 백준코딩/다이나믹 프로그래밍/12865 평범한 배낭.py
@@ -0,0 +1,18 @@
+#https://www.acmicpc.net/problem/12865
+
+n, k = map(int, input().split())
+
+#아이템 갯수별 원하는 무게에 따른 최대가치 dp 테이블 
+dp = [[0] * (k + 1) for i in range(n + 1)]
+
+# goods= (무게, 가치)블, 2차원 테이블
+goods = [tuple(map(int, input().split())) for _ in range(n)]
+
+for i in range(1, n+1):
+    for w in range(1, k+1):
+        if goods[i-1][0] > w:
+            dp[i][w] = dp[i-1][w]
+        else:
+            dp[i][w] = max(dp[i-1][w], goods[i-1][1] + dp[i-1][w - goods[i-1][0]])
+
+print(dp[n][k])
