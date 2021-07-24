fe2d48bd46b9bd5306080b3677fec74f793a84cd
Slowth-KIM <rt.slowth@gmail.com>
2021-07-24 21:13:21 +0900

    1005 ACM Craft 해결

--- 백준코딩/다이나믹 프로그래밍/1005 ACM Craft.py
+++ 백준코딩/다이나믹 프로그래밍/1005 ACM Craft.py
@@ -1,11 +1,44 @@
 #https://www.acmicpc.net/problem/1005
 
-T = int(input())
+from collections import deque
+from copy import deepcopy
+import sys
 
-n, k = map(int, input().split())
-building = list(map(int, input().split()))
+input = sys.stdin.readline
 
-indegree = [[] for i in range(n+1)]
+t = int(input())
 
-for _ in range(n):
-    pass
+for _ in range(t):
+    n, k = map(int, input().split())
+    building = [0] + list(map(int, input().split()))
+    
+    graph = [[] for i in range(n + 1)]
+    indegree = [0] * (n + 1)
+
+    queue = deque()
+
+    for _ in range(k):
+        x, y = map(int, input().split())
+        graph[x].append(y)
+        indegree[y] += 1
+
+    win = int(input())
+
+    for i in range(1, n + 1):
+        if indegree[i] == 0:
+            queue.append(i)
+
+    result = deepcopy(building)
+
+    while queue:
+        now = queue.popleft()
+        if now == win:
+            break
+
+        for i in graph[now]:
+            result[i] = max(result[i], result[now] + building[i])
+            indegree[i] -= 1
+            if indegree[i] == 0:
+                queue.append(i)
+
+    print(result[win])
--- 백준코딩/다이나믹 프로그래밍/12865 평범한 배낭.py
+++ 백준코딩/다이나믹 프로그래밍/12865 평범한 배낭.py
@@ -5,7 +5,7 @@ n, k = map(int, input().split())
 #아이템 갯수별 원하는 무게에 따른 최대가치 dp 테이블
 dp = [[0] * (k + 1) for i in range(n + 1)]
 
-# goods= (무게, 가치)블, 2차원 테이블
+# goods= (무게, 가치), 2차원 테이블
 goods = [tuple(map(int, input().split())) for _ in range(n)]
 
 for i in range(1, n+1):
