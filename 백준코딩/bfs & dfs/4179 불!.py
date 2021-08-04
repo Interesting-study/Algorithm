cca716ee67494002c7d93244408f36fc01b57b56
Slowth-KIM <rt.slowth@gmail.com>
2021-08-04 12:34:36 +0900

    4179 불 개선

--- 백준코딩/bfs & dfs/4179 불!.py
+++ 백준코딩/bfs & dfs/4179 불!.py
@@ -1,32 +1,64 @@
 from collections import deque
+from copy import deepcopy
 
-def find_location(maze, wanted):
-    for i in range(r):
-        for j in range(c):
-            if maze[i][j] == wanted:
-                return (i, j)
+def bfs():
 
-def bfs(x, y, what):
-    pass
+    while fire_q:
+        x, y = fire_q.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx < 0 or ny < 0 or nx >= r or ny >= c:
+                continue
+
+            if fire_visited[nx][ny] != 0 or maze[nx][ny] == "#":
+                continue
+
+            fire_visited[nx][ny] = fire_visited[x][y] + 1
+            fire_q.append((nx, ny))
+
+    while jihoon_q:
+        x, y = jihoon_q.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx < 0 or ny < 0 or nx >= r or ny >= c:
+                return jihoon_visited[x][y] + 1
+
+            if jihoon_visited[nx][ny] != 0 or maze[nx][ny] == "#" or (fire_visited[nx][ny] != 0 and fire_visited[nx][ny] <= jihoon_visited[x][y] + 1):
+                continue
+
+
+            jihoon_visited[nx][ny] = jihoon_visited[x][y] + 1
+            jihoon_q.append((nx, ny))
+
+    return 'IMPOSSIBLE'
 
 r, c = map(int, input().split())
 maze = [list(input()) for _ in range(r)]
 
+fire_q = deque()
+jihoon_q = deque()
 
+fire_visited = [[0] * c for _ in range(r)]
+jihoon_visited = [[0] * c for _ in range(r)]
 
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
 
+
 for i in range(r):
     for j in range(c):
-        if maze[i][j] == "#":
-            continue
-        else:
-            bfs(i, j, maze[i][j])
-            print(maze, end="\n\n")
+        if maze[i][j] == "F":
+            fire_q.append((i, j))
+        elif maze[i][j] == "J":
+            jihoon_q.append((i, j))
 
-print(start, fire)
-
+print(bfs())
 """
 4 4
 ####
