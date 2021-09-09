922bd253cf5f8d8805b116e280f59394069e3b6d
Slowth-KIM <rt.slowth@gmail.com>
2021-09-09 12:28:00 +0900

    프로그래머스 고득점 kit DFS/BFS

---
+++ programmers/그 외/타겟 넘버.py
@@ -0,0 +1,21 @@
+#https://programmers.co.kr/learn/courses/30/lessons/43165
+from collections import deque
+
+def solution(numbers, target):
+    answer = 0
+    q = deque()
+    q.append((0, 0))
+
+    while q:
+        current_sum, num_idx = q.popleft()
+        if num_idx == len(numbers):
+            if current_sum == target:
+                answer += 1
+        else:
+            number = numbers[num_idx]
+            q.append((current_sum + number, num_idx+1))
+            q.append((current_sum - number, num_idx + 1))
+
+    return answer
+
+print(solution([1, 1, 1, 1, 1], 3))
