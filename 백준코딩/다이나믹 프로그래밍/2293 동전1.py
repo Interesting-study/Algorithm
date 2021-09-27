8380ffd93483352ca75f227ff3ae007e48f0e9af
Slowth-KIM <rt.slowth@gmail.com>
2021-09-27 23:40:56 +0900

    2293 동전1 해결

--- 백준코딩/다이나믹 프로그래밍/2293 동전1.py
+++ 백준코딩/다이나믹 프로그래밍/2293 동전1.py
@@ -1,1 +1,13 @@
-#acmicpc.net/problem/2293
+#https://www.acmicpc.net/problem/2293
+n, k = map(int, input().split())
+coin_value = [int(input()) for _ in range(n)]
+
+dp = [0] * (k+1)
+
+dp[0] = 1
+
+for value in coin_value:
+    for idx in range(value, k+1):
+        dp[idx] += dp[idx-value]
+
+print(dp[k])
