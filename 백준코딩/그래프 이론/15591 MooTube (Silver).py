e5ad844435d946ca563981ca14f270be423dee5a
Slowth-KIM <rt.slowth@gmail.com>
2021-10-02 17:15:36 +0900

    15591 Mootube 해결

--- 백준코딩/그래프 이론/15591 MooTube (Silver).py
+++ 백준코딩/그래프 이론/15591 MooTube (Silver).py
@@ -1,15 +1,32 @@
 #https://www.acmicpc.net/problem/15591
-from collections import defaultdict
+from collections import defaultdict, deque
+import sys
 
-N, Q = map(int, input().split())
-usado = defaultdict(list)
+def count_related(k, v):
+    q = deque()
+    q.append((v, int(1e9)))
+    cnt = 0
+    visited = [False] * (N+1)
+
+    while q:
+        cv, c_usado = q.popleft()
+
+        if not visited[cv] and c_usado >= k:
+            visited[cv] = True
+            q.extend(videos[cv])
+
+    cnt = visited.count(True)
+    return cnt - 1
+
+N, Q = map(int, sys.stdin.readline().split())
+videos = defaultdict(list)
 
 for _ in range(N-1):
-    p, q, r = map(int, input().split())
-    usado[p-1].append((q-1, r))
-    usado[q-1].append((p-1, r))
+    p, q, r = map(int, sys.stdin.readline().split())
+    videos[p].append((q, r))
+    videos[q].append((p, r))
 
 for _ in range(Q):
-    k, v = map(int, input().split())
-
-print(usado)
+    #k가 유사도, v는 비디오 번호
+    k, v = map(int, sys.stdin.readline().split())
+    print(count_related(k, v))
