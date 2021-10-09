bdcc41b66f3024418646f9ef3b8771ffa124631a
Slowth-KIM <rt.slowth@gmail.com>
2021-10-09 22:44:03 +0900

    파일 생성

---
+++ programmers/그 외/빛의 경로 사이클.py
@@ -0,0 +1,9 @@
+#https://programmers.co.kr/learn/courses/30/lessons/86052
+def solution(grid):
+    answer = []
+    return answer
+
+
+print(solution(["SL","LR"]))
+print(solution(["S"]))
+print(solution(["R","R"]))
