90d55804863a277b5ca51f54a363ab06fd77a3c5
Slowth-KIM <rt.slowth@gmail.com>
2021-10-13 22:57:12 +0900

    22994 이미지 축소 해결

--- 백준코딩/구현/22994 이미지 축소.py
+++ 백준코딩/구현/22994 이미지 축소.py
@@ -1,10 +1,51 @@
 #https://www.acmicpc.net/problem/22994
-from collections import deque, OrderedDict
-
+INF = int(1e9)
 ni, mj = map(int, input().split())
-pixels = [deque(list(input()))  for _ in range(ni)]
+pixels = [list(input()) for _ in range(ni)]
+reverse_pixels = list(map(list, zip(*pixels)))
 
-same_pixel = deque()
-cnt_pixel = 0
+i_cnt = 1001
+j_cnt = 1001
+
+for i in range(ni):
+    now = pixels[i][0]
+    cnt = 1
+    for j in range(1, mj):
+        if now == pixels[i][j]:
+            cnt += 1
+        else:
+            j_cnt = min(j_cnt, cnt)
+            cnt = 1
+            now = pixels[i][j]
+
+if j_cnt == 1001:
+    j_cnt = mj
+
+if mj % j_cnt != 0:
+    j_cnt = 1
+
+for i in range(mj):
+    now = reverse_pixels[i][0]
+    cnt = 1
+
+    for j in range(1, ni):
+        if now == reverse_pixels[i][j]:
+            cnt += 1
+        else:
+            i_cnt = min(i_cnt, cnt)
+            cnt = 1
+            now = reverse_pixels[i][j]
+
+if i_cnt == 1001:
+    i_cnt = ni
+
+if ni % i_cnt != 0:
+    i_cnt = 1
+
+print(ni // i_cnt, mj // j_cnt, sep=" ")
+for i in range(0, ni, i_cnt):
+    for j in range(0, mj, j_cnt):
+        print(pixels[i][j], end='')
+    print()
 
 
