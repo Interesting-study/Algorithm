7c4ecdede91b31362dec68ed816ab4007abdbaa4
Slowth-KIM <rt.slowth@gmail.com>
2021-10-17 16:42:37 +0900

    17780 데이터 정제

--- 백준코딩/구현/17780 새로운 게임.py
+++ 백준코딩/구현/17780 새로운 게임.py
@@ -1,1 +1,13 @@
 #https://www.acmicpc.net/problem/17780
+#0은 흰색, 1은 빨간색, 2는 파란색
+from collections import defaultdict
+
+n, k = map(int, input().split())
+board = [input().split() for _ in range(n)]
+chess_piece = defaultdict(list)
+
+for i in range(1, k+1):
+    row, col, direction = map(int, input().split())
+    chess_piece[i].append([(row, col), direction])
+
+print(chess_piece[1])
