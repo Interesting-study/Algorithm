45ee3067e6336d04b54a4b8997b49086468b5211
Slowth-KIM <rt.slowth@gmail.com>
2021-10-18 23:06:20 +0900

    12100 2048 해결중

--- 백준코딩/구현/12100 2048 easy!.py
+++ 백준코딩/구현/12100 2048 easy!.py
@@ -1,1 +1,18 @@
 #https://www.acmicpc.net/problem/12100
+from copy import deepcopy
+INF = int(-1e9)
+
+n = int(input())
+board = [list(map(int, input().split()))for _ in range(n)]
+
+def get_max_block():
+    max_value = INF
+
+    for idx in range(n):
+        max_value = max(max_value, max(board[idx]))
+
+    return max_value
+
+now_max = get_max_block()
+
+print(now_max)
