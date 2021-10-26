efe5a8c38521bc47a0f115f67fc244f324ee95b3
Slowth-KIM <rt.slowth@gmail.com>
2021-10-26 20:54:05 +0900

    프로그래머스 뉴스 클러스터링 해결

--- programmers/카카오 코테/2018/뉴스 클러스터링.py
+++ programmers/카카오 코테/2018/뉴스 클러스터링.py
@@ -1,17 +1,50 @@
 #https://programmers.co.kr/learn/courses/30/lessons/17677
+from collections import Counter, defaultdict
+from math import floor
+
 def solution(str1, str2):
     answer = 65536
 
     str1 = str1.lower()
     str2 = str2.lower()
 
-    new_str1 = [str1[i:i+2] for i in range(0, len(str1), 2)]
-    new_str2 = [str2[i:i+2] for i in range(0, len(str2), 2)]
+    new_str1 = []
+    new_str2 = []
 
-    #합집합을 어케 구현할지
-    print(new_str1, new_str2)
+    for i in range(len(str1)-1):
+        keyword = str1[i:i+2]
+        if keyword.isalpha():
+            new_str1.append(keyword)
 
-    return
+    for i in range(len(str2)-1):
+        keyword = str2[i:i+2]
+        if keyword.isalpha():
+            new_str2.append(keyword)
+
+    if not new_str1 and not new_str2:
+        return answer
+
+    new_str1_cnt = Counter(new_str1)
+    new_str2_cnt = Counter(new_str2)
+
+    intersect = defaultdict(int)
+    union = defaultdict(int)
+
+    all_keys = list(set(new_str1 + new_str2))
+
+    for key in all_keys:
+        if key in new_str1_cnt.keys() and key in new_str2_cnt.keys():
+            intersect[key] = min(new_str1_cnt[key], new_str2_cnt[key])
+            union[key] = max(new_str1_cnt[key], new_str2_cnt[key])
+        elif key in new_str1_cnt.keys() and key not in new_str2_cnt.keys():
+            union[key] = new_str1_cnt[key]
+        elif key not in new_str1_cnt.keys() and key in new_str2_cnt.keys():
+            union[key] = new_str2_cnt[key]
+
+    jacquard = sum(intersect.values()) / sum(union.values())
+    answer = floor(answer * jacquard)
+
+    return answer
 
 print(solution("FRANCE", "french"))
 print(solution("handshake",	"shake hands"))
