c19573519247422a5263ee1e68ae708b19f70313
Slowth-KIM <rt.slowth@gmail.com>
2021-11-09 11:07:11 +0900

    leetcode Jewels and Stones 리스트 컴프리헨션

--- leetcode/771) Jewels and Stones.py
+++ leetcode/771) Jewels and Stones.py
@@ -1,6 +1,7 @@
 #https://leetcode.com/problems/jewels-and-stones/
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        """
         jewel_in_cnt = {j: 0 for j in jewels}
 
         for s in stones:
@@ -8,3 +9,5 @@ class Solution:
                 jewel_in_cnt[s] += 1
 
         return sum(jewel_in_cnt.values())
+        """
+        return sum(s in jewels for s in stones)
