26ec04cbef7a26fc8f6aa28f58615c9e72ec074f
Slowth-KIM <rt.slowth@gmail.com>
2021-11-13 10:21:41 +0900

    HR Staircase 해결

---
+++ HackerRank/Staircase.py
@@ -0,0 +1,24 @@
+#https://www.hackerrank.com/challenges/staircase/problem
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'staircase' function below.
+#
+# The function accepts INTEGER n as parameter.
+#
+
+def staircase(n):
+    for i in range(1, n+1):
+        stair = '#'*i
+        print(stair.rjust(n))
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    staircase(n)
