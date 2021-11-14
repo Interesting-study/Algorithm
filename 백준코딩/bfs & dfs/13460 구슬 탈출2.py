8d02421a7f0af7c1bb712ddefee4d77e263055dc
Slowth-KIM <rt.slowth@gmail.com>
2021-11-14 22:36:01 +0900

    백준코딩 문제 해결중

--- 백준코딩/bfs & dfs/13460 구슬 탈출2.py
+++ 백준코딩/bfs & dfs/13460 구슬 탈출2.py
@@ -9,7 +9,7 @@ def find_red(board):
 
 def find_exit(board):
     for i in range(n):
-        if 'R' in board[i]:
+        if 'O' in board[i]:
             j = board[i].index('O')
             break
     return (i, j)
@@ -17,26 +17,56 @@ def find_exit(board):
 def bfs(start):
     q = deque()
     q.append(start)
+    cnt = 0
 
     while q:
         x, y = q.popleft()
+
+        print('좌표, x:{}, y:{}'.format(x, y))
 
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
 
+            if nx < 0 or ny < 0 or nx >= n or ny >= m:
+                continue
+
             if board[nx][ny] == '#' or board[nx][ny] == "B":
                 continue
 
             if board[nx][ny] == "." and not visited[nx][ny]:
-                while board[nx][ny] == "." and not visited[nx][ny]:
+                cnt += 1
+
+                while board[nx][ny] == '.' or board[nx][ny] == 'O':
+                    print(nx, ny)
                     visited[nx][ny] = True
+
+                    if board[nx][ny] == 'O':
+                        return '\n' + str(cnt) + '번'
+
+                    nx += dx[i]
+                    ny += dy[i]
+
+                print("방향 : ", i)
+                qx = nx - dx[i]
+                qy = ny - dy[i]
+                q.append((qx, qy))
+
+                print("\n---\n")
+                break
+
+
+        print(q)
+
+
+
 
 
 n, m = map(int, input().split())
 board = [list(input()) for _ in range(n)]
 visited = [[False] * m for _ in range(n)]
 
+#상하좌우
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
--- 백준코딩/구현/17780 새로운 게임.py
+++ 백준코딩/구현/17780 새로운 게임.py
@@ -1,17 +1,19 @@
 #https://www.acmicpc.net/problem/17780
 #0은 흰색, 1은 빨간색, 2는 파란색
-from collections import defaultdict
+from collections import defaultdict, deque
+import string
 
 n, k = map(int, input().split())
 board = [input().split() for _ in range(n)]
 chess_piece = defaultdict(list)
+coord_in_piece = defaultdict(deque)
 
 #1 좌, 2 우, 3 상, 4 하
 dir_dict = {1: [0, -1], 2: [0, 1], 3: [-1, 0], 4: [1, 0]}
 
 for i in range(1, k+1):
     row, col, direction = map(int, input().split())
-    chess_piece[i].append((row, col))
-    chess_piece[i].append(direction)
+    chess_piece[string.ascii_uppercase[i-1]].append((row, col))
+    chess_piece[string.ascii_uppercase[i-1]].append(direction)
 
 print(chess_piece)
