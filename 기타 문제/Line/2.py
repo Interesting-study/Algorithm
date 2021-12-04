8709fa4cb9175b020712972085a6a29de290d20c
Slowth-KIM <rt.slowth@gmail.com>
2021-12-04 15:02:52 +0900

    문제 해결중

---
+++ 기타 문제/Line/2.py
@@ -0,0 +1,8 @@
+def solution(arr):
+    answer = 0
+    return answer
+
+print(solution([0, 1, 2, 5, 3, 7]))
+print(solution([1, 2, 3, 2, 1]))
+print(solution([1, 2, 3, 2, 1, 4, 3, 2, 2, 1]))
+print(solution([1, 2, 1, 2, 1]))~~~~~~~~~~~
