b1f9bffe32ab29a406bdab15482774043f4f9ad8
Slowth-KIM <rt.slowth@gmail.com>
2021-12-07 23:22:12 +0900

    13505 주유소 조건식 점검

--- 백준코딩/그리디 알고리즘/13305 주유소.py
+++ 백준코딩/그리디 알고리즘/13305 주유소.py
@@ -3,15 +3,30 @@
 n = int(input())
 len_info = list(map(int, input().split())) + [0]
 oil_info = list(map(int, input().split()))
-dp = [0] * (n+1)
 
-price = 0
-distance = sum(len_info)
-cheap = min(oil_info)
+price = oil_info[0] * len_info[0]
+distance = sum(len_info[1:])
+cheap = min(oil_info[:-1])
 
-for idx in range(n-1):
+for idx in range(1, n-1):
     now, next = idx, idx+1
 
+    if oil_info[now] == cheap:
+        price += (distance * cheap)
+        break
+
+    if oil_info[now] * (len_info[now] + len_info[next]):
+        pass
 
 
+print(price)
+
+
+'''
+4
+2 3 1
+5 3 4 1
+
+10, 12 = 22
+'''
 
