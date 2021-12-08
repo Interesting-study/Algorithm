919882a9178d356f1d976df1f8e79e710a6b0ee2
Slowth-KIM <rt.slowth@gmail.com>
2021-12-08 11:04:58 +0900

    13505 주유소 해결

--- 백준코딩/그리디 알고리즘/13305 주유소.py
+++ 백준코딩/그리디 알고리즘/13305 주유소.py
@@ -1,26 +1,20 @@
 #https://www.acmicpc.net/problem/13305
 
 n = int(input())
-len_info = list(map(int, input().split())) + [0]
+len_info = list(map(int, input().split()))
 oil_info = list(map(int, input().split()))
 
-price = oil_info[0] * len_info[0]
-distance = sum(len_info[1:])
-cheap = min(oil_info[:-1])
+res = 0
+min_price = oil_info[0]
 
-for idx in range(1, n-1):
-    now, next = idx, idx+1
+for i in range(n-1):
 
-    if oil_info[now] == cheap:
-        price += (distance * cheap)
-        break
+    if oil_info[i] < min_price:
+        min_price = oil_info[i]
 
-    if oil_info[now] * (len_info[now] + len_info[next]):
-        pass
+    res += min_price * len_info[i]
 
-
-print(price)
-
+print(res)
 
 '''
 4
