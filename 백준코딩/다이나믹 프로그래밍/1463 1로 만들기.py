1cb6879766fe6a22dca85ba17bee46d17eb5bb59
Slowth-KIM <rt.slowth@gmail.com>
2021-12-16 18:03:40 +0900

    1463 1로 만들기 - 해결

--- 백준코딩/다이나믹 프로그래밍/1463 1로 만들기.py
+++ 백준코딩/다이나믹 프로그래밍/1463 1로 만들기.py
@@ -1,5 +1,15 @@
 #https://www.acmicpc.net/problem/1463
 MAX = int(1e6) + 1
 n = int(input())
-
 dp = [0] * MAX
+
+for i in range(2, n+1):
+    dp[i] = dp[i - 1] + 1
+
+    if i % 2 == 0:
+        dp[i] = min(dp[i], dp[i // 2] + 1)
+
+    if i % 3 == 0:
+        dp[i] = min(dp[i], dp[i // 3] + 1)
+
+print(dp[n])
