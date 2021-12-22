0ca00259c0b6803d8c78f82ddb386290db34f665
Slowth-KIM <rt.slowth@gmail.com>
2021-12-22 21:39:21 +0900

    PR 수식 최대화 - 파일 생성

---
+++ programmers/카카오 코테/2020/수식 최대화.py
@@ -0,0 +1,7 @@
+#https://programmers.co.kr/learn/courses/30/lessons/67257
+def solution(expression):
+    answer = 0
+    return answer
+
+print(solution("100-200*300-500+20"))
+print(solution("50*6-3*2"))
