de66cddd0dec2f44de822ad903e7f5b592997cbc
Slowth-KIM <rt.slowth@gmail.com>
2021-12-24 20:51:58 +0900

    PR 수식 최대화 - 분할 정복 분석중

--- programmers/카카오 코테/2020/수식 최대화.py
+++ programmers/카카오 코테/2020/수식 최대화.py
@@ -1,6 +1,50 @@
 #https://programmers.co.kr/learn/courses/30/lessons/67257
+from itertools import permutations
+
+def calc(op, seq, exp):
+    if exp.isdigit():
+        return str(exp)
+    else:
+        if op[seq] == '*':
+            split_exp = exp.split('*')
+            temp = []
+            print(split_exp)
+            for s in split_exp:
+                temp.append(calc(op, seq+1, s))
+            print(temp)
+            print('-------------\n\n')
+            return str(eval('*'.join(temp)))
+
+        if op[seq] == '+':
+            split_exp = exp.split('+')
+            temp = []
+            print(split_exp)
+            for s in split_exp:
+                temp.append(calc(op, seq + 1, s))
+            print(temp)
+            print('-------------\n\n')
+            return str(eval('*'.join(temp)))
+
+        if op[seq] == "-":
+            split_exp = exp.split("-")
+            temp = []
+            for s in split_exp:
+                temp.append(calc(op, seq + 1, s))
+            print(temp)
+            print('-------------\n\n')
+            return str(eval("-".join(temp)))
+
+
 def solution(expression):
     answer = 0
+    op_rank = list(permutations(['*', '+', '-'], 3))
+
+    for op in op_rank:
+        print(op)
+        result = abs(int(calc(op, 0, expression)))
+
+        answer = max(answer, result)
+
     return answer
 
 print(solution("100-200*300-500+20"))
