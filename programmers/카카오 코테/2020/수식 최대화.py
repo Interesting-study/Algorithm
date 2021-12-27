9917fd794524447a5e3f39ac4180d842b5850b7a
Slowth-KIM <rt.slowth@gmail.com>
2021-12-27 20:52:47 +0900

    PR 수식 최대화 - 이해완료

--- programmers/카카오 코테/2020/수식 최대화.py
+++ programmers/카카오 코테/2020/수식 최대화.py
@@ -8,31 +8,23 @@ def calc(op, seq, exp):
         if op[seq] == '*':
             split_exp = exp.split('*')
             temp = []
-            print(split_exp)
             for s in split_exp:
-                print(s)
                 temp.append(calc(op, seq+1, s))
-            print(temp)
-            print('-------------\n\n')
+
             return str(eval('*'.join(temp)))
 
         if op[seq] == '+':
             split_exp = exp.split('+')
             temp = []
-            print(split_exp)
             for s in split_exp:
                 temp.append(calc(op, seq + 1, s))
-            print(temp)
-            print('-------------\n\n')
-            return str(eval('*'.join(temp)))
+            return str(eval('+'.join(temp)))
 
         if op[seq] == "-":
             split_exp = exp.split("-")
             temp = []
             for s in split_exp:
                 temp.append(calc(op, seq + 1, s))
-            print(temp)
-            print('-------------\n\n')
             return str(eval("-".join(temp)))
 
 def solution(expression):
@@ -40,9 +32,7 @@ def solution(expression):
     op_rank = list(permutations(['*', '+', '-'], 3))
 
     for op in op_rank:
-        print(op)
         result = abs(int(calc(op, 0, expression)))
-
         answer = max(answer, result)
 
     return answer
