14bd5cd113d19fef40f9e0513b41e429ae4ca163
Slowth-KIM <rt.slowth@gmail.com>
2022-01-06 19:57:38 +0900

    [Docs] BOJ - 개똥벌레, 범위설정

--- 백준코딩/이분 탐색/3020 개똥벌레.py
+++ 백준코딩/이분 탐색/3020 개똥벌레.py
@@ -1,4 +1,7 @@
+from collections import defaultdict
+
 n, h = map(int, input().split())
 obstacle = [int(input()) for _ in range(n)]
+broken_obstacle = defaultdict(list)
 
-left, right = 0, len(obstacle)
+left, right = 0, h
