5a9b87b04590ebb9456ea10bbb10e664cf0b6fe5
Slowth-KIM <rt.slowth@gmail.com>
2022-01-09 20:59:17 +0900

    [Feat] BOJ 3020 개똥벌레 - 이진탐색으로 해결, 누적합 X

--- 백준코딩/이분 탐색/3020 개똥벌레.py
+++ 백준코딩/이분 탐색/3020 개똥벌레.py
@@ -1,7 +1,32 @@
-from collections import defaultdict
+import sys
+from bisect import bisect_left
 
-n, h = map(int, input().split())
-obstacle = [int(input()) for _ in range(n)]
-broken_obstacle = defaultdict(list)
+def search():
+    min_value = float('inf')
+    cnt = 1
 
-left, right = 0, h
+    for i in range(1, h+1):
+        up_idx, down_idx = bisect_left(up_obstacle, (h+1)-i), bisect_left(down_obstacle, i)
+        obs_cnt = n - (up_idx + down_idx)
+        if obs_cnt < min_value:
+            min_value = obs_cnt
+            cnt = 1
+        elif obs_cnt == min_value:
+            cnt += 1
+
+    return [min_value, cnt]
+
+n, h = map(int, sys.stdin.readline().split())
+up_obstacle = []
+down_obstacle = []
+
+for idx in range(n):
+    if idx % 2 == 0:
+        down_obstacle.append(int(sys.stdin.readline().strip()))
+    else:
+        up_obstacle.append(int(sys.stdin.readline().strip()))
+
+up_obstacle.sort()
+down_obstacle.sort()
+
+print(*search())
