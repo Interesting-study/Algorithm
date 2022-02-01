eddf3990995b4ac926912baa5420c95a6dc0c487
Slowth-KIM <rt.slowth@gmail.com>
2022-02-01 18:41:07 +0900

    [Feat] 문제 파일 생성

---
+++ programmers/카카오 코테/2022/양궁대회.py
@@ -0,0 +1,9 @@
+def solution(n, info):
+    answer = []
+    return answer
+
+
+print(solution(5, [2,1,1,1,0,0,0,0,0,0,0]))
+print(solution(1, 	[1,0,0,0,0,0,0,0,0,0,0]))
+print(solution(9, 	[0,0,1,2,0,1,1,1,1,1,1]))
+print(solution(10, [0,0,0,0,0,0,0,0,3,4,3]))
