36227557e8fba2defc6fb43cd29df20bd53b904c
Slowth-KIM <rt.slowth@gmail.com>
2022-02-09 08:31:01 +0900

    [Feat] 카카오 2022 - 양과 늑대, 해결완료

--- programmers/카카오 코테/2022/양과 늑대.py
+++ programmers/카카오 코테/2022/양과 늑대.py
@@ -1,16 +1,45 @@
 #https://programmers.co.kr/learn/courses/30/lessons/92343
+import sys
+sys.setrecursionlimit(10**6)
+answer = 1
+
 def solution(info, edges):
-    answer = 0
-    edges_len = len(info) - 1
-    graph = [[] for _ in range(edges_len)]
-    sheep, wolf = 1, 0
+    global answer
+    global visited
+    info_len = len(info)
+    graph = [[] for _ in range(info_len)]
 
-    for i in range(edges_len):
-        parent, child = edges[i][0], edges[i][1]
+    for edge in edges:
+        parent, child = edge[0], edge[1]
         graph[parent].append(child)
+        graph[child].append(parent)
 
-    print(graph)
-    return answer
+    visited = [False] * info_len
+
+    def dfs(node, sheep, wolf, visited, node_set):
+        global answer
+        sheep += info[node] ^ 1
+        wolf += info[node]
+        print("노드 양 늑대 : ", node, sheep, wolf)
+
+        if wolf >= sheep:
+            return
+
+        if answer < sheep:
+            answer = sheep
+
+        next_visited = visited[:]
+        next_visited[node] = True
+
+        for next_node in node_set:
+            if not next_visited[next_node]:
+                next_node_set = node_set | set(graph[next_node])
+                dfs(next_node, sheep, wolf, next_visited, next_node_set)
+        return answer
+
+    return 'answer = ' + str(dfs(0, 0, 0, visited, set(graph[0]))) + '\n'
+
+
 
 print(solution([0,0,1,1,1,0,1,0,1,0,1,1], [[0,1],[1,2],[1,4],[0,8],[8,7],[9,10],[9,11],[4,3],[6,5],[4,6],[8,9]]))
-print(solution([0,1,0,1,1,0,1,0,0,1,0], [[0,1],[0,2],[1,3],[1,4],[2,5],[2,6],[3,7],[4,8],[6,9],[9,10]]))
+#print(solution([0,1,0,1,1,0,1,0,0,1,0], [[0,1],[0,2],[1,3],[1,4],[2,5],[2,6],[3,7],[4,8],[6,9],[9,10]]))
