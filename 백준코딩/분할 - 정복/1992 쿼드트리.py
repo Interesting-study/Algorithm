e55f4e1846e160b216b939544810d8612cbca212
Slowth-KIM <rt.slowth@gmail.com>
2022-02-08 17:58:32 +0900

    [Feat] 백준 1992 - 해결완료

---
+++ 백준코딩/분할 - 정복/1992 쿼드트리.py
@@ -0,0 +1,36 @@
+#https://www.acmicpc.net/problem/1992
+
+n = int(input())
+videos = [list(map(int, list(input()))) for _ in range(n)]
+answer = []
+
+def compress(x, y, n):
+    now = videos[x][y]
+
+    for i in range(x, x+n):
+        for j in range(y, y+n):
+            if now != videos[i][j]:
+                answer.append('(')
+                compress(x, y, n //2)
+                compress(x, y+n//2, n//2)
+                compress(x+n//2, y, n//2)
+                compress(x+n//2, y+n//2, n//2)
+                answer.append(')')
+                return
+
+    answer.append(str(now))
+
+
+compress(0, 0, n)
+print(''.join(answer))
+'''
+8
+11110000
+11110000
+00011100
+00011100
+11110000
+11110000
+11110011
+11110011
+'''
