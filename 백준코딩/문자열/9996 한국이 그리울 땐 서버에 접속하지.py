599e6cc10ba13e6adc992430e71c2a46a43ac1a3
Slowth-KIM <rt.slowth@gmail.com>
2022-03-03 08:51:21 +0900

    [Feat] 백준 9996 한국이 그리울 땐 서버에 접속하지 - 해결

---
+++ 백준코딩/문자열/9996 한국이 그리울 땐 서버에 접속하지.py
@@ -0,0 +1,15 @@
+#https://www.acmicpc.net/problem/9996
+import re
+
+n = int(input())
+pattern = input()
+files = [input() for _ in range(n)]
+
+re_pattern = re.compile(pattern.replace('*', '.*'))
+
+for file in files:
+    now = re_pattern.search(file)
+    if now and now.group() == file:
+        print('DA')
+    else:
+        print('NE')
