cdea8edb76ccf2529025ee75c86f58e1764cf56d
Slowth-KIM <rt.slowth@gmail.com>
2022-03-28 08:38:38 +0900

    [Feat] 22859 HTML 파싱 - 해결, 정규식 좀 더 공부 필요

---
+++ 백준코딩/22859 HTML 파싱.py
@@ -0,0 +1,18 @@
+#https://www.acmicpc.net/problem/22859
+import re
+
+html_doc = input()
+html_doc = html_doc[len('<main>'): -len('</main>')]
+
+html_doc = re.sub(r'<div +title="([\w ]*)">', r'title : \1\n', html_doc)
+html_doc = re.sub(r'</div>', '', html_doc)
+
+
+html_doc = re.sub(r'<p>', '', html_doc)
+html_doc = re.sub(r'</p>', '\n', html_doc)
+
+html_doc = re.sub(r'</?[\w ]*>', '', html_doc)
+html_doc = re.sub(r' ?\n ?', '\n', html_doc)
+html_doc = re.sub(r' {2,}', ' ', html_doc)
+
+print(html_doc)
