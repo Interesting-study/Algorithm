4e7bb5e45a4b69c100d86ed918b682fb4badb684
Slowth-KIM <rt.slowth@gmail.com>
2022-04-17 21:09:23 +0900

    [chore] 파일 수정

--- 백준코딩/다이나믹 프로그래밍/1695 팰린드롬 만들기.py
+++ 백준코딩/다이나믹 프로그래밍/1695 팰린드롬 만들기.py
@@ -1,8 +1,13 @@
 #https://www.acmicpc.net/problem/1695
-from collections import Counter
-
 n = int(input())
 arr = list(map(int, input().split()))
-arr_count = Counter(arr)
+dp = [[0]*(n+1) for _ in range(n+1)]
 
-print(arr_count)
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if arr[-i] == arr[j-1]:
+            dp[i][j] = dp[i-1][j-1] + 1
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+print(n - dp[-1][-1])
