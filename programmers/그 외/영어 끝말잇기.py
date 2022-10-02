5a1285a020540ed47c9cb68b0c8db7ebfe173f41
Slowth-KIM <rt.slowth@gmail.com>
2022-10-02 20:43:55 +0900

    [Feat] PR 영어 끝말잇기

---
+++ programmers/그 외/영어 끝말잇기.py
@@ -0,0 +1,12 @@
+def solution(n, words):
+    for i in range(1, len(words)):
+        if words[i][0] != words[i-1][-1] or words[i] in words[:i]:
+            return [(i%n)+1, (i//n)+1]
+    else:
+        return [0, 0]
+
+
+print(solution(3, ["tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"]))
+print(solution(5, ["hello", "observe", "effect", "take", "either", "recognize", "encourage", "ensure", "establish", "hang", "gather", "refer", "reference", "estimate", "executive"]))
+print(solution(2, ["hello", "one", "even", "never", "now", "world", "draw"]))
+print(solution(2, ["aba", "aba"]))
